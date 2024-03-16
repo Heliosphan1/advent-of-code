78f07db53977a6e2377558cd53d0782bc4e3b19a
Heliosphan1 <glavchanskiy.vitaliy@gmail.com>
2024-03-17 01:40:31 +0300

    2023-9; 2023-10 p1

--- 2023/09/aoc_2023_09.py
+++ 2023/09/aoc_2023_09.py
@@ -1,4 +1,58 @@
 # Advent of Code - 2023 - day 9
 
-with open("input.txt") as input_file:
-    input = input_file.read()
+def extrapolate(arr: list[int], reverse: bool = False) -> int:
+    done = False
+    if reverse:
+        last_diffs = [arr[0]]
+    else:
+        last_diffs = [arr[-1]]
+    c_seq = []
+    p_seq = arr
+    while not done:
+        if len(p_seq) == 1:
+            last_diffs.append(p_seq[0])
+            done = True
+        else:
+            done = True
+            diff = p_seq[1] - p_seq[0]
+            for i in range(1, len(p_seq)):
+                new_diff = p_seq[i] - p_seq[i-1]
+                c_seq.append(new_diff)
+                done = new_diff == diff
+            if reverse:
+                last_diffs.append(c_seq[0])
+            else:
+                last_diffs.append(c_seq[-1])
+            p_seq = list(c_seq)
+            c_seq = []
+
+    if reverse:
+        res = 0
+        for i, x in enumerate(last_diffs):
+            if not (i % 2):
+                res += x
+            else:
+                res -= x
+    else:
+        res = sum(last_diffs)
+    return res
+
+def solve_part_one():
+    with open("input.txt") as input_file:
+        res = 0
+        for line in input_file.readlines():
+            arr = list(map(int, line.split(' ')))
+            res += extrapolate(arr, reverse=False)
+        print(res)
+    return res
+
+def solve_part_two():
+    with open("input.txt") as input_file:
+        res = 0
+        for line in input_file.readlines():
+            arr = list(map(int, line.split(' ')))
+            res += extrapolate(arr, reverse=True)
+        print(res)
+    return res
+
+solve_part_two()
--- 2023/10/aoc_2023_10.py
+++ 2023/10/aoc_2023_10.py
@@ -1,4 +1,67 @@
 # Advent of Code - 2023 - day 10
+   
 
+field = []
 with open("input.txt") as input_file:
-    input = input_file.read()
+    for i, line in enumerate(input_file.readlines()):
+        pos = line.find('S')
+        if pos >= 0:
+            start = (pos, i)
+        field.append(line.strip('\n'))
+
+dim_x = len(field[0]) - 1
+dim_y = len(field) - 1
+
+pipe_path = {
+    (1, 0): {
+        'J': (0, -1),
+        '7': (0, 1),
+        '-': (1, 0)
+    },
+    (-1, 0): {
+        'F': (0, 1),
+        'L': (0, -1),
+        '-': (-1, 0)
+    },
+    (0, 1): {
+        'J': (-1, 0),
+        'L': (1, 0),
+        '|': (0, 1) 
+    },
+    (0, -1): {
+        'F': (1, 0),
+        '7': (-1, 0),
+        '|': (0, -1)
+    }
+}
+
+def search_first(node):
+    x, y = node
+    for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        x1, y1 = x + dx, y + dy
+        if (0 <= x1 <= dim_x) and (0 <= y1 <= dim_y) and (field[y1][x1] in pipe_path[(dx, dy)]):
+                return (x1, y1)
+            
+def search_next(node, prev_node):
+    x, y = node
+    x0, y0 = prev_node
+    dx, dy = x - x0, y - y0
+    dx1, dy1 = pipe_path[(dx, dy)][field[y][x]]
+    return (x + dx1, y + dy1)
+
+steps = 1
+on_path = set(start)
+prev_node = start
+node = search_first(start)
+while True:
+    on_path.add(node)
+    next_node = search_next(node, prev_node)
+    if field[next_node[1]][next_node[0]] == 'S':
+        break
+    else:
+        steps += 1
+        prev_node = node
+        node = next_node
+        
+part1_ans = steps//2 + steps % 2 
+print(steps//2 + steps % 2)
